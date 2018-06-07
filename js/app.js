3ac7445f7eaa8c263e5fed890ec214f3b93830b9
Chris Hawk <chrishawk9@gmail.com>
2018-06-07 13:42:14 -0600

    feat: add marker for each restaurant

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
 class Restaurant{
-    constructor(name, lat, long) {
+    constructor(name, lat, lng) {
         this.name = name;
         this.lat = lat;
-        this.long = long;
+        this.lng = lng;
     }
 }
 
@@ -17,7 +17,7 @@ let hardCodedRestaurantList = [
     ];
 var map;
 
-init();
+//init();
 
 function RestaurantListViewModel() {
     let self = this;
@@ -31,31 +31,35 @@ function RestaurantListViewModel() {
  * Initialize the app by getting a map of the neighbourhood and a list of restaurants in that
  * neighbourhood using Google and Yelp API calls.
  */
-function init() {
-    //get map form Google maps API
-    //start the server
-
-    //get restaurants from Yelp via server
-    //let restaurantData;
-    fetch('http://localhost:3000/').then(response => {
-        return response.blob();//.json.toString();
-        // restaurantData = response;
-        // console.log(restaurantData);
-    }).then(blob => {
-        let reader = new FileReader();
-        reader.readAsText(blob);
-        let restaurantData = reader.result;
-        console.log(restaurantData);
-        restaurantData.jsonBody.businesses.forEach(restaurant => console.log(restaurant.id));
-    });
-    //console.log(restaurantData);
-
-}
+// function init() {
+//     //get map form Google maps API
+//     //start the server
+//
+//     //get restaurants from Yelp via server
+//     //let restaurantData;
+//     fetch('http://localhost:3000/').then(response => {
+//         return response.blob();//.json.toString();
+//         // restaurantData = response;
+//         // console.log(restaurantData);
+//     }).then(blob => {
+//         let reader = new FileReader();
+//         reader.readAsText(blob);
+//         let restaurantData = reader.result;
+//         console.log(restaurantData);
+//         restaurantData.jsonBody.businesses.forEach(restaurant => console.log(restaurant.id));
+//     });
+//     //console.log(restaurantData);
+//
+// }
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 51.0601364, lng: -114.1827787},
         zoom: 15
+    });
+    //Add a marker for each restaurant
+    hardCodedRestaurantList.forEach(restaurant => {
+        let marker = new google.maps.Marker({position: {lat: restaurant.lat, lng: restaurant.lng}, map: map});
     });
 }
 
